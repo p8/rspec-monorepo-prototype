47f3ba86e8f4bea05ba41492a619f7a3799d0dba
Petrik <petrik@deheus.net>
2020-01-30 17:41:43 +0100

    Skip rspec-rails

--- script/functions.sh
+++ script/functions.sh
@@ -188,7 +188,7 @@ function run_all_spec_suites {
   fold "rspec-core specs" run_spec_suite_for "rspec-core"
   fold "rspec-expectations specs" run_spec_suite_for "rspec-expectations"
   fold "rspec-mocks specs" run_spec_suite_for "rspec-mocks"
-  fold "rspec-rails specs" run_spec_suite_for "rspec-rails"
+  #fold "rspec-rails specs" run_spec_suite_for "rspec-rails"
 
   if rspec_support_compatible; then
     fold "rspec-support specs" run_spec_suite_for "rspec-support"
